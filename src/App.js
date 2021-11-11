f0fac5c7d21eacdd34977c2c18386e8fa2546447
ZèD <imzihad@gmail.com>
2021-11-11 15:15:31 +0600

    Initialized authentication skeletons
    Signed-off-by: ZèD <imzihad@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import {
   BrowserRouter as Router,
   Switch,
-  Route
+  Route,
+  Redirect
 } from "react-router-dom";
 import Home from './Pages/Home/Home/Home';
 import Header from './Pages/Shared/Header/Header';
 import Footer from './Pages/Shared/Footer/Footer';
 import NavBar from './Pages/Shared/NavBar/NavBar';
 import Products from './Pages/Products/Products/Products';
+import Authentication from './Pages/Authentication/Authentication/Authentication';
 
 function App() {
   return (
@@ -18,7 +20,13 @@ function App() {
         <NavBar />
         <Switch>
           <Route exact path="/">
+            <Redirect to='/home' />
+          </Route>
+          <Route path="/home">
             <Home />
+          </Route>
+          <Route path="/authentication">
+            <Authentication />
           </Route>
           <Route path="/products">
             <Products />
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div className='mx-10 flex justify-between'>
-            <div className='h-32'>
-                <img className='w-full h-full' src="https://i.ibb.co/mSrC2Xp/header-logo.png" alt="" />
+            <div className='h-28'>
+                <Link to='/home'>
+                    <img className='w-full h-full' src="https://i.ibb.co/mSrC2Xp/header-logo.png" alt="" />
+                </Link>
             </div>
             <div className='my-auto rounded-lg'>
                 {
-                    <button className='text-white bg-pink-700 hover:bg-pink-600 px-5 py-3 rounded-lg'><i class="fas fa-sign-in-alt"></i> Sign In</button>
+                    <Link to='/authentication'>
+                        <button className='text-white bg-pink-700 hover:bg-pink-600 px-5 py-3 rounded-lg'><i class="fas fa-sign-in-alt"></i> Sign In</button>
+                    </Link>
                 }
                 <button className='text-white bg-pink-700 hover:bg-pink-600 px-5 py-3 rounded-lg ml-2'><i className="fas fa-search"></i> Search</button>
                 <button className='text-white bg-pink-700 hover:bg-pink-600 px-5 py-3 rounded-lg ml-2'><i className="fas fa-shopping-cart"></i> Cart</button>
