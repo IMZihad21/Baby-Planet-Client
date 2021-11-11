8b22a626b0cab54d8bf90e88edd40f057c1589aa
ZèD <imzihad@gmail.com>
2021-11-11 12:55:02 +0600

    Replace check details with single purchase page
    Signed-off-by: ZèD <imzihad@gmail.com>

--- src/Pages/Products/ProductDetails/ProductDetails.js
+++ src/Pages/Products/ProductDetails/ProductDetails.js
@@ -9,10 +9,7 @@ const ProductDetails = ({ product }) => {
             </div>
             <h1 className='text-2xl mt-2 text-pink-700'>{productName}</h1>
             <p className='mt-1'>{productDescription.split(' ').slice(0, 10).toString().replace(/,/g, ' ') + '...'}</p>
-            <div className='bg-pink-700 rounded-lg divide-x divide-white mt-2'>
-                <button className='text-white py-2 pr-2'><i class="fas fa-info-circle"></i> Check Details</button>
-                <button className='text-white py-2 pl-2'><i class="fas fa-cart-plus"></i> Add to Cart</button>
-            </div>
+            <button className='bg-pink-700 rounded-lg mt-2 w-full text-white py-2'><i class="fas fa-cart-plus"></i> Purchase</button>
         </div>
     );
 };
