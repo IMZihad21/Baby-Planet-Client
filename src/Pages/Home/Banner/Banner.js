163108d4a58a1ada6b262bfa51a9cabe7c745b9a
ZèD <imzihad@gmail.com>
2021-11-11 10:57:38 +0600

    Misc changes in UI
    Signed-off-by: ZèD <imzihad@gmail.com>

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -4,26 +4,31 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const Banner = () => {
     return (
-        <Carousel
-            className='mt-5'
-            showStatus={false}
-            showIndicators={false}
-            showThumbs={false}
-            swipeable={false}
-            animationHandler='fade'
-            autoPlay={true}
-            infiniteLoop={true}
-        >
+        <div className='mt-5'>
             <div>
-                <img class="w-full h-full rounded-lg" src="https://i.ibb.co/v1Vsmsk/banner-1.png" alt="bannerimage" />
+                <h1 className='text-3xl text-left font-semibold text-right my-10'>Make <span className='text-pink-700 font-bold'>Memories ...</span></h1>
             </div>
-            <div>
-                <img class="w-full h-full rounded-lg" src="https://i.ibb.co/N7pRdCm/banner-2.png" alt="bannerimage" />
-            </div>
-            <div>
-                <img class="w-full h-full rounded-lg" src="https://i.ibb.co/k3xJyK4/banner-3.png" alt="bannerimage" />
-            </div>
-        </Carousel>
+            <Carousel
+                className='mt-5'
+                showStatus={false}
+                showIndicators={false}
+                showThumbs={false}
+                swipeable={false}
+                animationHandler='fade'
+                autoPlay={true}
+                infiniteLoop={true}
+            >
+                <div>
+                    <img class="w-full h-full rounded-lg" src="https://i.ibb.co/v1Vsmsk/banner-1.png" alt="bannerimage" />
+                </div>
+                <div>
+                    <img class="w-full h-full rounded-lg" src="https://i.ibb.co/N7pRdCm/banner-2.png" alt="bannerimage" />
+                </div>
+                <div>
+                    <img class="w-full h-full rounded-lg" src="https://i.ibb.co/k3xJyK4/banner-3.png" alt="bannerimage" />
+                </div>
+            </Carousel>
+        </div>
     );
 };
 
--- src/Pages/Home/TopProducts/TopProducts.js
+++ src/Pages/Home/TopProducts/TopProducts.js
@@ -7,13 +7,15 @@ const TopProducts = () => {
     useEffect(() => {
         axios.get('http://localhost:9000/products')
             .then((response) => {
-                setProducts(response.data);
+                const data = response.data;
+                const slicedData = data.slice(0, 6);
+                setProducts(slicedData);
             });
     }, [])
     return (
         <div className='mt-5'>
             <div>
-                <h1 className='text-3xl text-left font-semibold'>Top <span className='text-pink-700 font-bold'>Sales</span></h1>
+                <h1 className='text-3xl text-left font-semibold my-10'>Top <span className='text-pink-700 font-bold'>Sales ...</span></h1>
             </div>
             <div className='mt-2 bg-pink-100 rounded-lg p-5 grid grid-cols-4 gap-3'>
                 {
